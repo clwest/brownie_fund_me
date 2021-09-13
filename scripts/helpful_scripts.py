7a5629d4428cc8a65da794844b0bd2bb22a12f43
clwest <westremarketing@gmail.com>
2021-09-13 16:28:20 -0600

    first commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,22 @@
+from brownie import network, config, accounts, MockV3Aggregator
+from web3 import Web3
+
+DECIMALS = 8
+STARTING_PRICE = 20000000000000
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+def get_account():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying mocks")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
+    price_feed_address = MockV3Aggregator[-1].address
+    print("Mocks Deployed") 
